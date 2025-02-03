a693d9bf8df5e627bb242d5ea48f0c5b09ab0667
ESLAMELSAADANI <144673435+ESLAMELSAADANI@users.noreply.github.com>
2025-02-03 23:09:37 +0200

    Q01 - The Bubble Sort algorithm has a time complexity of O(n^2) in its worst and average cases

---
+++ Assignment/Helper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal static class Helper<T> where T : IComparable<T>
+    {
+        #region Q01 - The Bubble Sort algorithm has a time complexity of O(n^2) in its worst and average cases
+
+        public static void SWAP(ref T x, ref T y)
+        {
+            T z = x;
+            x = y;
+            y = z;
+        }
+
+        public static void OptimizedBubbleSort(T[] arr)
+        {
+            if (arr is null || arr.Length <= 0)
+                return;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                bool Swapped = false;
+                for (int j = 0; j < arr.Length - 1 - i; j++)
+                {
+                    if (arr[j].CompareTo(arr[j + 1]) == 1)
+                        SWAP(ref arr[j], ref arr[j + 1]);
+                    Swapped = true;
+                }
+                if (!Swapped)
+                    break;
+            }
+        }
+
+        #endregion
+    }
+}
--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,38 +1,13 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics.Metrics;
 
 namespace Assignment
 {
     internal class Program
     {
-
-        #region Q01 - The Bubble Sort algorithm has a time complexity of O(n^2) in its worst and average cases
-
-        public static void BubbleSortV02(int[] arr)
-        {
-            int length = arr.Length;
-            bool isSwapped;
-            for (int i = 0; i < length - 1; i++)
-            {
-                isSwapped = false;
-                for (int j = 0; j < length - i - 1; j++)
-                {
-                    if (arr[j] > arr[j + 1])
-                    {
-                        int temp = arr[j];
-                        arr[j] = arr[j + 1];
-                        arr[j + 1] = temp;
-                        isSwapped = true;
-                    }
-                }
-                // If no elements were swapped, the array is already sorted
-                if (!isSwapped)
-                    break;
-            }
-        }
-
-        #endregion
 
         #region Q03 - You are given an ArrayList containing a sequence of elements.
 
@@ -104,11 +79,11 @@ namespace Assignment
             #region Q01 - The Bubble Sort algorithm has a time complexity of O(n^2) in its worst and average cases
 
             //int[] arr = { 64, 34, 25, 12, 22, 11, 90 };
-            //BubbleSortV02(arr);
+            //Helper<int>.OptimizedBubbleSort(arr);
 
             //foreach (int num in arr)
             //{
-            //    Console.Write(num+" ");
+            //    Console.Write(num+" ");// 11 12 22 25 34 64 90
             //}
 
             #endregion
